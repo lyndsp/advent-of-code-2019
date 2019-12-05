3b32a44ae877bdc5ed7693aaf13cf42966f30196
lyndsp <lyndsayp@gmail.com>
2019-12-05 20:24:44 +0000

    Answer to day (in debug mode).

--- day1.go
+++ day1.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
 // CalculateFuelRequired calculates fuel for given mass
 func CalculateFuelRequired(mass int) int {
 	return (mass / 3) - 2
@@ -16,4 +23,34 @@ func SumFuelForMasses(masses []int) int {
 }
 
 func main() {
+	file, err := os.Open("day1-input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	var masses = []int{}
+	for _, line := range lines {
+		var mass = 0
+
+		mass, err := strconv.Atoi(line)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		masses = append(masses, mass)
+	}
+
+	var totalFuel = SumFuelForMasses(masses)
+
+	println("Total fuel is: ", totalFuel)
 }
